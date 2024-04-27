3eaa9647b7f780320e2aad88ac7f515b0373e3e4
wpeterashworth <wingsaseagles12@gmail.com>
2024-04-26 22:04:00 -0600

    Finished C# prep1-2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,91 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        string letter = "";
+        string sign = "";
+        int grade = int.Parse(gradeString);
+        Console.WriteLine();
+        if (grade >= 93)
+        {
+            letter = "A";
+            sign = "";
+        }
+        if (grade >= 90)
+        {
+            letter = "A";
+            sign = "-";
+        }
+        else if (grade >= 87)
+        {
+            letter = "B";
+            sign = "+";
+        }
+        else if (grade >= 83)
+        {
+            letter = "B";
+            sign = "";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            sign = "-";
+        }
+        else if (grade >= 77)
+        {
+            letter = "C";
+            sign = "+";
+        }
+        else if (grade >= 73)
+        {
+            letter = "C";
+            sign = "";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            sign = "-";
+        }
+        else if (grade >= 67)
+        {
+            letter = "D";
+            sign = "+";
+        }
+        else if (grade >= 63)
+        {
+            letter = "D";
+            sign = "";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            sign = "-";
+        }
+        else
+        {
+            letter = "F";
+        }
+        List<string> lettersWithA = ["B", "C", "D"];
+        if (lettersWithA.Contains(letter))
+        {
+            Console.Write($"You received a {letter}{sign} letter grade. ");
+        }
+        else
+        {
+            Console.Write($"You received an {letter}{sign} letter grade. ");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry but you did not pass. ");
+        }
     }
 }
